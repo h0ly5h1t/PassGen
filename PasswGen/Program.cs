e81932195e203a57eca0b98fb400fb375ec173b9
h0ly5h1t <124813325+h0ly5h1t@users.noreply.github.com>
2023-02-27 18:13:43 +0300

    Add files via upload

--- PasswGen/Program.cs
+++ PasswGen/Program.cs
@@ -1,53 +1,48 @@
 ﻿using System;
 
-namespace PasswGen
+namespace Password_Generator
 {
     internal class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
+            bool passTrue = true;
             Console.WriteLine("Введите длину пароля:");
-            try
+            int.TryParse(Console.ReadLine(), out int symCount);
+            while (passTrue)
             {
-                int symCount = int.Parse(Console.ReadLine());
-                // Проверка длины пароля
                 if (symCount < 8)
                 {
-                    Console.ForegroundColor = ConsoleColor.Red;
-                    Console.WriteLine("Минимальная длина пароля - 8 символов!");
+                    Console.WriteLine("Минимальная длина пароля составляет - 8 символов!");
+                    Console.ReadKey();
+                    passTrue = false;
                 }
                 else
                 {
-                    while (true)
-                    {
-                        Console.WriteLine($"Ваш пароль - \u001b[32;1m{GeneratePassword(symCount)}\u001b[0m\nДлина пароля - {symCount}");
-                        Console.WriteLine("Закройте программу или нажмите любую клавишу, чтобы сгенерировать новый пароль!");
-                        Console.ReadKey();
-                        Console.Clear();
-                    }
+                    Console.WriteLine($"Ваш пароль - \u001b[32;1m{GeneratePassword(symCount)}\u001b[0m\nДлина пароля - {symCount}");
+                    Console.WriteLine("Закройте программу или нажмите любую клавишу, чтобы сгенерировать новый пароль!");
+                    Console.ReadKey();
+                    Console.Clear();
                 }
+
             }
-            catch (FormatException)
-            {
-                Console.ForegroundColor = ConsoleColor.Red;
-                Console.WriteLine("Пошел вон отсюда, кыш негодяй! Не балуйся!");
-            }
+
         }
 
-        static string GeneratePassword(int symCount)
+        private static string GeneratePassword(int symCount)
         {
             string password = "";
-            Random rnd = new Random();
+            Random randomChars = new Random();
+
             char[] charList ={'A','a','B','b','C','c','D','d','E','e','F','f','G','g','H','h','I',
             'i','J','j','K','k','L','l','M','m','N','n','O','o','P','p','Q','q','R','r','S','s',
             'T','t','U','u','V','v','W','w','X','x','Y','y','Z','z','1','2','3','4','5','6','7',
             '8','9','&', '@', '#', '$', '%', '^', '*', '(', ')', '!', '|', '+', '-', ':', '/',
             '.', ';', '`', '~'};
-            // Цикл добавления в пароль ,по заданной длине, символов из массива
+
             for (int i = 0; i < symCount; i++)
-            {
-                password += charList[rnd.Next(0, charList.Length - 1)];
-            }
+                password += charList[randomChars.Next(0, charList.Length)];
+
             return password;
         }
     }
